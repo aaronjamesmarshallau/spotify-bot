7e5a405e697e39dee5c4422793a57fae3c228c9a
aaronjamesmarshallau <hi@imaaronnicetomeetyou.me>
2017-05-27 22:11:38 +1000

    Adds sorting to search results by popularity
    Search results are now sorted by popularity

--- html/js/main.js
+++ html/js/main.js
@@ -148,6 +148,18 @@ $(document).ready(function () {
 					var results = [];
 					$(".search-results").empty();
 
+					tracks.sort(function (a, b) {
+						if (a.popularity < b.popularity) {
+							return 1;
+						}
+
+						if (a.popularity > b.popularity) {
+							return -1;
+						}
+
+						return 0;
+					});
+
 					for (var i = 0; i < tracks.length; i++) {
 						(function (index) {
 							var el = tracks[index];
